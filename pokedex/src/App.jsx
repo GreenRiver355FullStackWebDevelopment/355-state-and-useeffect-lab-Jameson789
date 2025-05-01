d0633b729718a2662b344374c7fb1111544382d8
Jameson Farmer <thejamesonfarmer@gmail.com>
2025-05-01 11:28:00 -0700

    Added fetch call and props being passed to cards.jsx

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+//import './App.css'
 
+import Cards from "./components/Cards";
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemon, setPokemon] = useState([]);
+  const [page, setPage] = useState([0]);
 
+  useEffect(() => {
+    fetchPokemon(page);
+  }, [page]);
+
+  const fetchPokemon = async (page) => {
+    try {
+      const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${page[0]}`);
+      const data = await res.json();
+      setPokemon(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  console.log(pokemon);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+      <h1>Pokemon List</h1>
+      <div className="main-container ">
+        <div>
+          {pokemon && <Cards pokemon={pokemon} />}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
---
+++ pokedex/src/components/Cards.jsx
@@ -0,0 +1,10 @@
+const Cards = ({ pokemon }) => {
+    console.log(pokemon.results);
+    return (
+        <div className="cards">
+            
+        </div>
+    )
+}
+
+export default Cards;
